d790017eae595ac9a57298fe0224cbda76711e78
murrellb <murrellb@gmail.com>
2025-07-16 00:40:06 +0200

    Adding transform to process.

--- src/coalescent_flow.jl
+++ src/coalescent_flow.jl
@@ -3,10 +3,12 @@
 #Need to restrict on lmask though:
 #@time a = element.((compound_state,), 1:seqlength(compound_state), 1);
 
-struct CoalescentFlow{Proc,D} <: Process
+struct CoalescentFlow{Proc,D,F} <: Process
     P::Proc
     branch_time_dist::D
+    split_transform::F
 end
+CoalescentFlow(P, branch_time_dist) = CoalescentFlow(P, branch_time_dist, x -> exp.(clamp.(x, -100, 11)))
 
 #We could move the lmask and cmask to the outer state wrapper, but that would mean the individual losses wouldn't dispatch correctly.
 #let's try this for now.
@@ -38,9 +40,9 @@ function possible_merges(nodes)
 end
 
 #This returns a forest, which is a vector of trees, annotated with anchors
-function sample_forest(P::CoalescentFlow, elements::AbstractVector; groupings = zeros(Int, length(elements)), flowable = ones(Bool, length(elements)))
-    nodes = FlowNode[FlowNode(1.0, elements[i], 1, groupings[i], flowable[i]) for i = 1:length(elements)]
-    coal_times = sort(rand(P.branch_time_dist, sum(possible_merges(nodes))), rev = true) #Because coal stuff walks backwards from 1
+function sample_forest(P::CoalescentFlow, elements::AbstractVector; groupings = zeros(Int, length(elements)), flowable = ones(Bool, length(elements)), T = Float32)
+    nodes = FlowNode[FlowNode(T(1), elements[i], 1, groupings[i], flowable[i]) for i = 1:length(elements)]
+    coal_times = T.(sort(rand(P.branch_time_dist, sum(possible_merges(nodes))), rev = true)) #Because coal stuff walks backwards from 1
     for time in coal_times
         ind = rand(findall(possible_merges(nodes)))
         left, right = nodes[ind], nodes[ind+1]
@@ -56,12 +58,12 @@ end
 #This takes an ordered vector of states and runs the bridge
 function tree_bridge(P::CoalescentFlow, node, Xs, target_t, current_t, collection)
     if !node.free #This means it won't matter what the X0 was if the node was frozen!
-        push!(collection, (;Xt = node.node_data, X1anchor = node.node_data, descendants = node.weight, cmasked = false, group = node.group))
+        push!(collection, (;Xt = node.node_data, X1anchor = node.node_data, descendants = node.weight, cmasked = false, group = node.group, last_coalescence = current_t))
         return
     end
     if node.time > target_t #<-If we're on the branch where a sample is needed
         Xt = bridge(P.P, Xs, node.node_data, current_t, target_t)
-        push!(collection, (;Xt, X1anchor = node.node_data, descendants = node.weight, cmasked = true, group = node.group))
+        push!(collection, (;Xt, X1anchor = node.node_data, descendants = node.weight, cmasked = true, group = node.group, last_coalescence = current_t))
     else
         next_Xs = bridge(P.P, Xs, node.node_data, current_t, node.time)
         for child in node.children
@@ -69,6 +71,7 @@ function tree_bridge(P::CoalescentFlow, node, Xs, target_t, current_t, collectio
         end
     end
 end
+#JUST ADDED last_coalescence = current_t which needs to prop through and get returned to the user in a nice shape to use for masking recent coal events
 
 #Runs a single bridge for each tree in the forest, when X1 is a (tuple of) tensor state(s), and aggregates the results across the trees in the forest
 #Needs to have "flowable" allow "nothing"
@@ -86,8 +89,8 @@ end
 #Takes a vector of (tuples of) tensor states, runs the bridges for each, and re-batches them and their anchors.
 #Assumes masked states for now.
 #function branching_bridge(P::CoalescentFlow, X0sampler, X1s::Vector{BranchingState}; t_sample = ()->rand(Float32))
-function branching_bridge(P::CoalescentFlow, X0sampler, X1s; t_sample = ()->rand(Float32))
-    times = [t_sample() for _ in 1:length(X1s)]
+function branching_bridge(P::CoalescentFlow, X0sampler, X1s; t_sample = ()->rand(Float32), T = Float32)
+    times = [T(t_sample()) for _ in 1:length(X1s)]
     batch_bridge = [forest_bridge(P, X0sampler, X1.state, times[i], X1.groupings, X1.state[1].cmask) for (i, X1) in enumerate(X1s)]
     maxlen = maximum(length.(batch_bridge))
     b = length(X1s)
@@ -109,6 +112,12 @@ function branching_bridge(P::CoalescentFlow, X0sampler, X1s; t_sample = ()->rand
             padmask[i,b] = true
         end
     end
+    prev_coalescence = zeros(T, maxlen, b)
+    for b in 1:length(batch_bridge)
+        for i in 1:length(batch_bridge[b])
+            prev_coalescence[i,b] = batch_bridge[b][i].last_coalescence
+        end
+    end
     groups = zeros(Int, maxlen, b)
     for b in 1:length(batch_bridge)
         for i in 1:length(batch_bridge[b])
@@ -118,7 +127,7 @@ function branching_bridge(P::CoalescentFlow, X0sampler, X1s; t_sample = ()->rand
     splits_target = split_target.((P,), times', clamp.(descendants .- 1, 0, Inf))
     Xt_batch = MaskedState.(regroup([[b.Xt for b in bridges] for bridges in batch_bridge]), (cmask,), (padmask .& cmask,));
     X1anchor_batch = MaskedState.(regroup([[b.X1anchor for b in bridges] for bridges in batch_bridge]), (cmask,), (padmask .& cmask,));
-    return (;t = times, Xt = BranchingState(Xt_batch, groups), X1anchor = X1anchor_batch, descendants, splits_target, freemask = cmask, padmask)
+    return (;t = times, Xt = BranchingState(Xt_batch, groups), X1anchor = X1anchor_batch, descendants, splits_target, freemask = cmask, padmask, prev_coalescence)
 end
 
 
@@ -169,10 +178,19 @@ function Flowfusion.step(P::CoalescentFlow, XₜBS::BranchingState, hat::Tuple, 
     time_remaining = (1-s₁)
     delta_t = s₂ - s₁
     X1targets, event_lambdas = hat
-    Xₜ = bridge(P.P, Xₜ, X1targets, s₁, s₂)
-    splits = rand.(Poisson.((delta_t*event_lambdas)/time_remaining))[:]
+    Xₜ = Flowfusion.step(P.P, Xₜ, X1targets, s₁, s₂)
+    splits = rand.(Poisson.((delta_t*P.split_transform.(event_lambdas))/time_remaining))[:]
     #Zero out split events for any non-continuous states that have changed.
-
+    for s in 1:length(XₜBS.state)
+        if (XₜBS.state[s] isa DiscreteState)
+            for i in 1:length(splits)
+                if element(tensor(Xₜ[s]),i,1) != element(tensor(XₜBS.state[s]),i,1)
+                    println("Discrete component $s changed at $i")
+                    splits[i] = 0
+                end
+            end
+        end
+    end
     current_length = size(tensor(first(Xₜ)))[end-1]
     new_length = current_length + sum(splits)
     element_tuple = element.(Xₜ, 1, 1)
--- src/loss.jl
+++ src/loss.jl
@@ -1,3 +1,9 @@
 poisson_loss(mu, count, mask) = sum(mask .* (mu .- xlogy.(count, mu))) / sum(mask)
 #This version has a min of zero, but identical gradients to the above. Makes the loss trajectory clearer but doesn't change the optimization.
 shifted_poisson_loss(mu, count, mask) = sum(mask .* (mu .- xlogy.(count, mu))) / sum(mask) - sum(mask .* (count .- xlogy.(count, count))) / sum(mask)
+
+#Bregman Poisson loss
+bpl(mu, count) = mu .- xlogy.(count, mu)
+sbpl(mu, count) = mu .- xlogy.(count, mu) .- (count .- xlogy.(count, count))
+Flowfusion.floss(P::CoalescentFlow, X̂₁, X₁, mask, c) = Flowfusion.scaledmaskedmean(sbpl(P.split_transform.(X̂₁), X₁), c, mask)
+#Design choice: should we enforce the splits to have a singleton dimension up front? Maybe yes, because then we can do splits, deaths, etc all in one tensor?
